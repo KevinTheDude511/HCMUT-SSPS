440d32a78e39a12fbed6ed8aa60686991f3daeec
NguyenMKhue <138444220+NguyenMKhue@users.noreply.github.com>
2023-12-06 10:31:11 +0700

    add Availability

--- js/buyPage.js
+++ js/buyPage.js
@@ -1,6 +1,6 @@
 updateCartQuantity();
-document.querySelectorAll('.increase').forEach((button) => {
-  button.addEventListener('click', () => {
+document.querySelectorAll(".increase").forEach((button) => {
+  button.addEventListener("click", () => {
     const paperID = button.dataset.counterId;
     let data;
     data = document.getElementById(paperID).value;
@@ -8,8 +8,8 @@ document.querySelectorAll('.increase').forEach((button) => {
     document.getElementById(paperID).value = data;
   });
 });
-document.querySelectorAll('.decrease').forEach((button) => {
-  button.addEventListener('click', () => {
+document.querySelectorAll(".decrease").forEach((button) => {
+  button.addEventListener("click", () => {
     const paperID = button.dataset.counterId;
     let data;
     data = document.getElementById(paperID).value;
@@ -17,8 +17,8 @@ document.querySelectorAll('.decrease').forEach((button) => {
     document.getElementById(paperID).value = data;
   });
 });
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  button.addEventListener('click', () => {
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
     const paperId = button.dataset.paperId;
     let matchingItem;
     cart.forEach((item) => {
@@ -26,17 +26,26 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         matchingItem = item;
       }
     });
-    counterId = paperId + '-counter';
+    counterId = paperId + "-counter";
     data = document.getElementById(counterId).value;
     if (matchingItem) {
-      if(Number(data)>0) {
-      matchingItem.quantity += Number(data);}
+      if (Number(data) > 0) {
+        if (matchingItem.limit - Number(data) < 0) {
+          alert('Number of purchases exceed limit!');
+        } else {
+          matchingItem.quantity += Number(data);
+          matchingItem.limit -= Number(data);
+        }
+        console.log(matchingItem.limit);
+      }
     } else {
-      if(Number(data)>0){
-      cart.push({
-        id: paperId,
-        quantity: Number(data),
-      })};
+      if (Number(data) > 0) {
+        cart.push({
+          id: paperId,
+          quantity: Number(data),
+          limit: 50 - Number(data),
+        });
+      }
     }
     saveToStorage();
     updateCartQuantity();
@@ -44,27 +53,26 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   });
 });
 function updateCartQuantity() {
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += 1;
-    });
-    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
-    updateQuantity();
-  }
+  let cartQuantity = 0;
+  cart.forEach((item) => {
+    cartQuantity += 1;
+  });
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  updateQuantity();
+}
 function updateQuantity() {
-    products.forEach((item) => {
-      let matchingItem;
-      cart.forEach((cartItem) => {
-        if (item.id === cartItem.id) {
-          matchingItem = cartItem;
-        }
-      });
-      paperId = "js-"+item.id+"-quantity";
-      if (matchingItem){
-        document.getElementById(paperId).innerHTML=matchingItem.quantity;
-      }
-      else {
-        document.getElementById(paperId).innerHTML=0;
+  products.forEach((item) => {
+    let matchingItem;
+    cart.forEach((cartItem) => {
+      if (item.id === cartItem.id) {
+        matchingItem = cartItem;
       }
     });
+    paperId = "js-" + item.id + "-quantity";
+    if (matchingItem) {
+      document.getElementById(paperId).innerHTML = matchingItem.quantity;
+    } else {
+      document.getElementById(paperId).innerHTML = 0;
+    }
+  });
 }
